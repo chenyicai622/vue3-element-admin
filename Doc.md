be502650584fd7083b973da697c8bdfcc583ec45
chenyicai <chenyicai622@qq.com>
2023-03-09 15:49:37 +0800

    feat(构建): 构建重置

---
+++ Doc.md
@@ -0,0 +1,6 @@
+- vscode配置
+```js
+"editor.codeActionsOnSave": {
+	"source.fixAll.eslint": true
+}
+```
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import path from 'path'
+
+const resolve = (dir: string) => path.resolve(__dirname, dir)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			'@': resolve('src')
+		}
+	}
 })
